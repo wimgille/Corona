f20b942b2d0b026205fe7da97d61e96c656f1173
wimgille <wimgille@MacBook-Pro-van-Wim.local>
2020-07-26 15:55:21 +0200

    Repairing Scraper Function

--- scraper.py
+++ scraper.py
@@ -53,7 +53,8 @@ def ExcessMort():
     # week 20 https://www.euromomo.eu/component---src-templates-graphs-and-maps-js-1fb13ae0d6683c6d2b12.js
     # week 21 https://www.euromomo.eu/component---src-templates-graphs-and-maps-js-c9af76ec69719f4fd96a.js
     # week 23 https://www.euromomo.eu/component---src-templates-graphs-and-maps-js-25b37370cda3f45b1802.js
-    
+    # week 30 https://www.euromomo.eu/component---src-templates-graphs-and-maps-js-32b0c7c01e7ecdb2bd68.js
+
 
     # strip the textfile and isolate the right JSON object
     sourceText = r.split('{e.exports=JSON.parse(')
@@ -62,31 +63,31 @@ def ExcessMort():
     data=json.loads(new)
 
     # prepare the colums in 50 different lists
-    date = data["weeks"]
+    date = data["pooled"]["weeks"]
 
-    zscore_0to4 = data["pooled"]["groups"][0]['zscore']
-    total_number_deaths_0to4 = data["pooled"]["groups"][0]['nbc']
-    baseline_deaths_0to4 = data["pooled"]["groups"][0]['pnb']
-    excess_deaths_0to4 = data["pooled"]["groups"][0]['excess']
-    si_deaths_0to4 = data["pooled"]["groups"][0]['si']
-    hnr_deaths_0to4 = data["pooled"]["groups"][0]['hnr']
-    lnr_deaths_0to4 = data["pooled"]["groups"][0]['lnr']
+    zscore_0to14 = data["pooled"]["groups"][0]['zscore']
+    total_number_deaths_0to14 = data["pooled"]["groups"][0]['nbc']
+    baseline_deaths_0to14 = data["pooled"]["groups"][0]['pnb']
+    excess_deaths_0to14 = data["pooled"]["groups"][0]['excess']
+    si_deaths_0to14 = data["pooled"]["groups"][0]['si']
+    hnr_deaths_0to14 = data["pooled"]["groups"][0]['hnr']
+    lnr_deaths_0to14 = data["pooled"]["groups"][0]['lnr']
 
-    zscore_15to64 = data["pooled"]["groups"][1]['zscore']
-    total_number_deaths_15to64 = data["pooled"]["groups"][1]['nbc']
-    baseline_deaths_15to64 = data["pooled"]["groups"][1]['pnb']
-    excess_deaths_15to64 = data["pooled"]["groups"][1]['excess']
-    si_deaths_15to64 = data["pooled"]["groups"][1]['si']
-    hnr_deaths_15to64 = data["pooled"]["groups"][1]['hnr']
-    lnr_deaths_15to64 = data["pooled"]["groups"][1]['lnr']
+    zscore_15to44 = data["pooled"]["groups"][1]['zscore']
+    total_number_deaths_15to44 = data["pooled"]["groups"][1]['nbc']
+    baseline_deaths_15to44 = data["pooled"]["groups"][1]['pnb']
+    excess_deaths_15to44 = data["pooled"]["groups"][1]['excess']
+    si_deaths_15to44 = data["pooled"]["groups"][1]['si']
+    hnr_deaths_15to44 = data["pooled"]["groups"][1]['hnr']
+    lnr_deaths_15to44 = data["pooled"]["groups"][1]['lnr']
 
-    zscore_5to14 = data["pooled"]["groups"][2]['zscore']
-    total_number_deaths_5to14 = data["pooled"]["groups"][2]['nbc']
-    baseline_deaths_5to14 = data["pooled"]["groups"][2]['pnb']
-    excess_deaths_5to14 = data["pooled"]["groups"][2]['excess']
-    si_deaths_5to14 = data["pooled"]["groups"][2]['si']
-    hnr_deaths_5to14 = data["pooled"]["groups"][2]['hnr']
-    lnr_deaths_5to14 = data["pooled"]["groups"][2]['lnr']
+    zscore_45to64 = data["pooled"]["groups"][2]['zscore']
+    total_number_deaths_45to64 = data["pooled"]["groups"][2]['nbc']
+    baseline_deaths_45to64 = data["pooled"]["groups"][2]['pnb']
+    excess_deaths_45to64 = data["pooled"]["groups"][2]['excess']
+    si_deaths_45to64 = data["pooled"]["groups"][2]['si']
+    hnr_deaths_45to64 = data["pooled"]["groups"][2]['hnr']
+    lnr_deaths_45to64 = data["pooled"]["groups"][2]['lnr']
 
     zscore_65P = data["pooled"]["groups"][3]['zscore']
     total_number_deaths_65P = data["pooled"]["groups"][3]['nbc']
@@ -130,9 +131,9 @@ def ExcessMort():
 
     # and merge all the list into a dataframe
     df=pd.DataFrame({"Date":date, "zscore_tot":zscore_tot, "total_number_deaths_tot":total_number_deaths_tot, "baseline_deaths_tot":baseline_deaths_tot, "excess_deaths_tot":excess_deaths_tot, "si_deaths_tot": si_deaths_tot, "hnr_deaths_tot":hnr_deaths_tot, "lnr_deaths_tot": lnr_deaths_tot, \
-        "zscore_0to4":zscore_0to4, "total_number_deaths_0to4":total_number_deaths_0to4, "baseline_deaths_0to4":baseline_deaths_0to4, "excess_deaths_0to4":excess_deaths_0to4, "si_deaths_0to4": si_deaths_0to4, "hnr_deaths_0to4":hnr_deaths_0to4, "lnr_deaths_0to4": lnr_deaths_0to4, \
-        "zscore_15to64":zscore_15to64, "total_number_deaths_15to64":total_number_deaths_15to64, "baseline_deaths_15to64":baseline_deaths_15to64, "excess_deaths_15to64":excess_deaths_15to64, "si_deaths_15to64": si_deaths_15to64, "hnr_deaths_15to64":hnr_deaths_15to64, "lnr_deaths_15to64": lnr_deaths_15to64, \
-        "zscore_5to14":zscore_5to14, "total_number_deaths_5to14":total_number_deaths_5to14, "baseline_deaths_5to14":baseline_deaths_5to14, "excess_deaths_5to14":excess_deaths_5to14, "si_deaths_5to14": si_deaths_5to14, "hnr_deaths_5to14":hnr_deaths_5to14, "lnr_deaths_5to14": lnr_deaths_5to14, \
+        "zscore_0to14":zscore_0to14, "total_number_deaths_0to14":total_number_deaths_0to14, "baseline_deaths_0to14":baseline_deaths_0to14, "excess_deaths_0to14":excess_deaths_0to14, "si_deaths_0to14": si_deaths_0to14, "hnr_deaths_0to14":hnr_deaths_0to14, "lnr_deaths_0to14": lnr_deaths_0to14, \
+        "zscore_15to44":zscore_15to44, "total_number_deaths_15to44":total_number_deaths_15to44, "baseline_deaths_15to44":baseline_deaths_15to44, "excess_deaths_15to44":excess_deaths_15to44, "si_deaths_15to44": si_deaths_15to44, "hnr_deaths_15to44":hnr_deaths_15to44, "lnr_deaths_15to44": lnr_deaths_15to44, \
+        "zscore_45to64":zscore_45to64, "total_number_deaths_45to64":total_number_deaths_45to64, "baseline_deaths_45to64":baseline_deaths_45to64, "excess_deaths_45to64":excess_deaths_45to64, "si_deaths_45to64": si_deaths_45to64, "hnr_deaths_45to64":hnr_deaths_45to64, "lnr_deaths_45to64": lnr_deaths_45to64, \
         "zscore_65P":zscore_65P, "total_number_deaths_65P":total_number_deaths_65P, "baseline_deaths_65P":baseline_deaths_65P, "excess_deaths_65P":excess_deaths_65P, "si_deaths_65P": si_deaths_65P, "hnr_deaths_65P":hnr_deaths_65P, "lnr_deaths_65P": lnr_deaths_65P, \
         "zscore_65to74":zscore_65to74, "total_number_deaths_65to74":total_number_deaths_65to74, "baseline_deaths_65to74":baseline_deaths_65to74, "excess_deaths_65to74":excess_deaths_65to74, "si_deaths_65to74": si_deaths_65to74, "hnr_deaths_65to74":hnr_deaths_65to74, "lnr_deaths_65to74": lnr_deaths_65to74, \
         "zscore_75to84":zscore_75to84, "total_number_deaths_75to84":total_number_deaths_75to84, "baseline_deaths_75to84":baseline_deaths_75to84, "excess_deaths_75to84":excess_deaths_75to84, "si_deaths_75to84": si_deaths_75to84, "hnr_deaths_75to84":hnr_deaths_75to84, "lnr_deaths_75to84": lnr_deaths_75to84, \
@@ -169,7 +170,7 @@ def FindURL():
     return targetURL
 
 if __name__ == "__main__":
-    source = FindURL()
+    source = ExcessMort()
     print(source)
     print('done')
     
